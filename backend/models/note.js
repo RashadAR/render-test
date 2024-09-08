e73cf77699165aa1982b3af0a418d3d1a7fd2f32
Rashad <rashad.rar37@gmail.com>
2024-09-08 11:21:49 +0530

    database add

--- backend/models/note.js
+++ backend/models/note.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
-console.log('connecting to', url)
 
 mongoose.connect(url)
     .then(result => {
